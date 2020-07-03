268dd237f10a885815074c4980c1fc1bb21c51b8
Ruwan Lokubalasooriya <ru1x89@gmail.com>
2020-07-03 13:01:01 +0530

    Add separate header & sidebar components - Update project readme file

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,81 @@
 <template>
-  <div id="app">
-    <router-view />
+  <div id="app" class="h-screen flex overflow-hidden bg-gray-100">
+    <MobileNav />
+
+    <div class="hidden md:flex md:flex-shrink-0">
+      <div
+        class="flex flex-col w-64 border-r border-gray-200 pt-5 pb-4 bg-white"
+      >
+        <div class="flex items-center flex-shrink-0 px-4">
+          <img
+            class="h-8 w-auto"
+            src="/img/opusxentax250.png"
+            alt="Immortals"
+          />
+        </div>
+
+        <div class="mt-5 h-0 flex-1 flex flex-col overflow-y-auto">
+          <Nav />
+        </div>
+      </div>
+    </div>
+
+    <div class="flex flex-col w-0 flex-1 overflow-hidden">
+      <div class="relative z-10 flex-shrink-0 flex h-16 bg-white shadow">
+        <button
+          class="px-4 border-r border-gray-200 text-gray-500 focus:outline-none focus:bg-gray-100 focus:text-gray-600 md:hidden"
+          aria-label="Open sidebar"
+        >
+          <svg
+            class="h-6 w-6"
+            stroke="currentColor"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h16M4 18h7"
+            />
+          </svg>
+        </button>
+
+        <Header />
+      </div>
+
+      <main
+        class="flex-1 relative z-0 overflow-y-auto py-6 focus:outline-none"
+        tabindex="0"
+      >
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+          <h1 class="text-2xl font-semibold text-gray-900">OPXT Components</h1>
+        </div>
+
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+          <div class="py-4">
+            <router-view />
+          </div>
+        </div>
+      </main>
+    </div>
   </div>
 </template>
+
+<script>
+import Nav from "@/components/Nav.vue";
+import Header from "@/components/Header.vue";
+import MobileNav from "@/components/MobileNav.vue";
+
+export default {
+  name: "App",
+  components: {
+    Nav,
+    Header,
+    MobileNav
+  }
+};
+</script>
 
 <style lang="scss">
 @import "./assets/scss/app.scss";
---
+++ src/components/MobileNav.vue
@@ -0,0 +1,163 @@
+<template>
+  <div class="md:hidden">
+    <div class="fixed inset-0 flex z-40">
+      <div class="fixed inset-0">
+        <div class="absolute inset-0 bg-gray-600 opacity-75"></div>
+      </div>
+
+      <div
+        class="relative flex-1 flex flex-col max-w-xs w-full pt-5 pb-4 bg-white"
+      >
+        <div class="absolute top-0 right-0 -mr-14 p-1">
+          <button
+            class="flex items-center justify-center h-12 w-12 rounded-full focus:outline-none focus:bg-gray-600"
+            aria-label="Close sidebar"
+          >
+            <svg
+              class="h-6 w-6 text-white"
+              stroke="currentColor"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </div>
+
+        <div class="flex-shrink-0 flex items-center px-4">
+          <img
+            class="h-8 w-auto"
+            src="/img/opusxentax250.png"
+            alt="Immortals"
+          />
+        </div>
+
+        <div class="mt-5 flex-1 h-0 overflow-y-auto">
+          <nav class="px-2">
+            <a
+              href="#"
+              class="group flex items-center px-2 py-2 text-base leading-6 font-medium text-gray-900 rounded-md bg-gray-100 focus:outline-none focus:bg-gray-200 transition ease-in-out duration-150"
+            >
+              <svg
+                class="mr-4 h-6 w-6 text-gray-500 group-hover:text-gray-500 group-focus:text-gray-600 transition ease-in-out duration-150"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path fill="none" d="M0 0h24v24H0z" />
+                <path
+                  fill="currentColor"
+                  d="M13.06 8.11l1.415 1.415a7 7 0 0 1 0 9.9l-.354.353a7 7 0 0 1-9.9-9.9l1.415 1.415a5 5 0 1 0 7.071 7.071l.354-.354a5 5 0 0 0 0-7.07l-1.415-1.415 1.415-1.414zm6.718 6.011l-1.414-1.414a5 5 0 1 0-7.071-7.071l-.354.354a5 5 0 0 0 0 7.07l1.415 1.415-1.415 1.414-1.414-1.414a7 7 0 0 1 0-9.9l.354-.353a7 7 0 0 1 9.9 9.9z"
+                />
+              </svg>
+              Link 1
+            </a>
+            <a
+              href="#"
+              class="mt-1 group flex items-center px-2 py-2 text-base leading-6 font-medium text-gray-600 rounded-md hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-100 transition ease-in-out duration-150"
+            >
+              <svg
+                class="mr-4 h-6 w-6 text-gray-400 group-hover:text-gray-500 group-focus:text-gray-500 transition ease-in-out duration-150"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path fill="none" d="M0 0h24v24H0z" />
+                <path
+                  fill="currentColor"
+                  d="M13.06 8.11l1.415 1.415a7 7 0 0 1 0 9.9l-.354.353a7 7 0 0 1-9.9-9.9l1.415 1.415a5 5 0 1 0 7.071 7.071l.354-.354a5 5 0 0 0 0-7.07l-1.415-1.415 1.415-1.414zm6.718 6.011l-1.414-1.414a5 5 0 1 0-7.071-7.071l-.354.354a5 5 0 0 0 0 7.07l1.415 1.415-1.415 1.414-1.414-1.414a7 7 0 0 1 0-9.9l.354-.353a7 7 0 0 1 9.9 9.9z"
+                />
+              </svg>
+              Link 2
+            </a>
+            <a
+              href="#"
+              class="mt-1 group flex items-center px-2 py-2 text-base leading-6 font-medium text-gray-600 rounded-md hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-100 transition ease-in-out duration-150"
+            >
+              <svg
+                class="mr-4 h-6 w-6 text-gray-400 group-hover:text-gray-500 group-focus:text-gray-500 transition ease-in-out duration-150"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path fill="none" d="M0 0h24v24H0z" />
+                <path
+                  fill="currentColor"
+                  d="M13.06 8.11l1.415 1.415a7 7 0 0 1 0 9.9l-.354.353a7 7 0 0 1-9.9-9.9l1.415 1.415a5 5 0 1 0 7.071 7.071l.354-.354a5 5 0 0 0 0-7.07l-1.415-1.415 1.415-1.414zm6.718 6.011l-1.414-1.414a5 5 0 1 0-7.071-7.071l-.354.354a5 5 0 0 0 0 7.07l1.415 1.415-1.415 1.414-1.414-1.414a7 7 0 0 1 0-9.9l.354-.353a7 7 0 0 1 9.9 9.9z"
+                />
+              </svg>
+              Link 3
+            </a>
+            <a
+              href="#"
+              class="mt-1 group flex items-center px-2 py-2 text-base leading-6 font-medium text-gray-600 rounded-md hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-100 transition ease-in-out duration-150"
+            >
+              <svg
+                class="mr-4 h-6 w-6 text-gray-400 group-hover:text-gray-500 group-focus:text-gray-500 transition ease-in-out duration-150"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path fill="none" d="M0 0h24v24H0z" />
+                <path
+                  fill="currentColor"
+                  d="M13.06 8.11l1.415 1.415a7 7 0 0 1 0 9.9l-.354.353a7 7 0 0 1-9.9-9.9l1.415 1.415a5 5 0 1 0 7.071 7.071l.354-.354a5 5 0 0 0 0-7.07l-1.415-1.415 1.415-1.414zm6.718 6.011l-1.414-1.414a5 5 0 1 0-7.071-7.071l-.354.354a5 5 0 0 0 0 7.07l1.415 1.415-1.415 1.414-1.414-1.414a7 7 0 0 1 0-9.9l.354-.353a7 7 0 0 1 9.9 9.9z"
+                />
+              </svg>
+              Link 4
+            </a>
+            <a
+              href="#"
+              class="mt-1 group flex items-center px-2 py-2 text-base leading-6 font-medium text-gray-600 rounded-md hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-100 transition ease-in-out duration-150"
+            >
+              <svg
+                class="mr-4 h-6 w-6 text-gray-400 group-hover:text-gray-500 group-focus:text-gray-500 transition ease-in-out duration-150"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path fill="none" d="M0 0h24v24H0z" />
+                <path
+                  fill="currentColor"
+                  d="M13.06 8.11l1.415 1.415a7 7 0 0 1 0 9.9l-.354.353a7 7 0 0 1-9.9-9.9l1.415 1.415a5 5 0 1 0 7.071 7.071l.354-.354a5 5 0 0 0 0-7.07l-1.415-1.415 1.415-1.414zm6.718 6.011l-1.414-1.414a5 5 0 1 0-7.071-7.071l-.354.354a5 5 0 0 0 0 7.07l1.415 1.415-1.415 1.414-1.414-1.414a7 7 0 0 1 0-9.9l.354-.353a7 7 0 0 1 9.9 9.9z"
+                />
+              </svg>
+              Link 5
+            </a>
+            <a
+              href="#"
+              class="mt-1 group flex items-center px-2 py-2 text-base leading-6 font-medium text-gray-600 rounded-md hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-100 transition ease-in-out duration-150"
+            >
+              <svg
+                class="mr-4 h-6 w-6 text-gray-400 group-hover:text-gray-500 group-focus:text-gray-500 transition ease-in-out duration-150"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path fill="none" d="M0 0h24v24H0z" />
+                <path
+                  fill="currentColor"
+                  d="M13.06 8.11l1.415 1.415a7 7 0 0 1 0 9.9l-.354.353a7 7 0 0 1-9.9-9.9l1.415 1.415a5 5 0 1 0 7.071 7.071l.354-.354a5 5 0 0 0 0-7.07l-1.415-1.415 1.415-1.414zm6.718 6.011l-1.414-1.414a5 5 0 1 0-7.071-7.071l-.354.354a5 5 0 0 0 0 7.07l1.415 1.415-1.415 1.414-1.414-1.414a7 7 0 0 1 0-9.9l.354-.353a7 7 0 0 1 9.9 9.9z"
+                />
+              </svg>
+              Link 5
+            </a>
+          </nav>
+        </div>
+      </div>
+
+      <div class="flex-shrink-0 w-14"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MobileNav"
+};
+</script>
